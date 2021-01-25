d0fd6c0a9843d5b39b2e7086869481d3b7707700
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-25 01:34:11 -0500

    Leccion 70

---
+++ pages/dogs/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Dogs = () => {
+    //const breeds = ['akita', 'boxer', 'bulldog'] 
+    const [breeds, setBreeds] = useState(['akita', 'boxer', 'bulldog'] )
+
+    const handleAdd = ()=>{
+       // setBreeds(breeds.push(...breeds)) // salta error TypeError: breeds.map is not a function, no sirve por que reemplaza el estado anterior, debe crear un nuevo objeto
+       setBreeds([...breeds,'bullterrier'])
+
+    }
+    return <>
+        <h2>Dogs</h2>
+        <hr />
+        <button onClick={handleAdd}>Agregar</button>
+        <ol>
+            {
+                //for(const breed of breeds)
+                //error no se puede esto
+                breeds.map(breed=>{
+                   // return <li>{breed}</li>
+                   // error anterior por falta de key, no colocar el valor del indice
+                   return <li key={breed}>{breed}</li> // sirve la key si no hay duplicados.
+                })
+            }
+        </ol>
+    </>
+}
+export default Dogs;
