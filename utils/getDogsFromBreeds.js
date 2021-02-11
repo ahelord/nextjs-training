fbf6745cf60ee92f42f531c444647cf8675676f5
Leonardo Rodriguez <leonardo@booklick.co>
2021-02-10 22:53:21 -0500

    Leccion 76 y 77

---
+++ utils/getDogsFromBreeds.js
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
+
+   export const getDogsFromBreeds = async(breed) =>{
+        const url = `https://dog.ceo/api/breed/${breed}/images/random/6`;
+        const request = await fetch(url);
+        const {message:imgDogs} = await request.json();
+        const dogs = imgDogs.map(imgDog=>{
+            return {
+                uuid:uuidv4(),
+                imgUrl:imgDog
+            }
+        })
+       return dogs;
+    }
