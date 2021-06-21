36ade7cc33cca56ace270c0d3b9011dd9105cd92
Leonardo Rodriguez <leonardo@booklick.co>
2021-06-20 22:07:26 -0500

    Utilziar multiples hooks leccion 111

--- components/MultiplesHooks.js
+++ components/MultiplesHooks.js
@@ -1,12 +1,42 @@
 import useFetch from "../hooks/useFetch";
+import {useCounter} from "../hooks/useState/useCounter";
 
 const MultiplesHooks = () => {
-    useFetch("https://www.breakingbadapi.com/api/quotes/1")
+    const {counter,increment,decrement} = useCounter(1)
+    const {loading,data} = useFetch("https://www.breakingbadapi.com/api/quotes/"+counter)
+    const {author,quote} = !!data && data[0];
     return (
         <>
             <div>
-                <h1>Hola multiples hooks</h1>
+                <h1>Breaking bads notas</h1>
+                <hr/>
             </div>
+
+            {
+                loading ?
+                    <>
+                        <div className="spinner-grow text-secondary" role="status">
+                        </div><br/>
+                    </> :
+
+                    <blockquote>
+                        <p className="mb-0">{quote}</p>
+                        <br></br>
+                        <footer className="blockquote-footer">{author}</footer>
+                    </blockquote>
+            }
+
+
+            {counter > 1 &&
+            <button onClick={()=>decrement()} className="btn-primary">
+                Nota anterior
+            </button>
+            }
+
+            <button onClick={()=>increment()} className="btn-primary">
+                Siguiente nota
+            </button>
+
 
         </>
     );
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -3,6 +3,11 @@ import {useEffect, useState} from 'react'
 const useFetch = (url) => {
     const [state, setState] = useState({data: null, loading: true, error: null});
     useEffect(async () => {
+        setState({
+            loading: true,
+            error: null,
+            data:null
+        })
         const request = await fetch(url);
         const data = await request.json();
         setState({
