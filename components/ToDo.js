46ca15be53bb3174d14d656e76bf795b83ff6093
Leonardo Rodriguez <leonardo.rodriguez@grability.com>
2021-08-02 22:42:59 -0500

    Leccion 127 agregar todo

--- components/ToDo.js
+++ components/ToDo.js
@@ -1,5 +1,6 @@
-import { useReducer } from "react"
+import {useReducer} from "react"
 import toDoReducer from "../reducers/toDoReducer"
+
 const initialState = [{
     id: new Date().getTime(),
     description: 'Aprender react',
@@ -12,16 +13,60 @@ const ToDo = () => {
 
     const [todos, dispatch] = useReducer(toDoReducer, initialState)
     console.log(todos)
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const newTodo = {
+            id: new Date().getTime(),
+            description: 'Aprender hook',
+            done: false
+        }
+        const action = {
+            type: 'add',
+            payload:newTodo
+        }
+        dispatch(action);
+    }
     return (
-        <div>
-            ToDo
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+        <>
+            <div className="col-5">
+                <h1>ToDo {todos.length}</h1>
+                <ul>
+                    {
+                        todos.map((todo, index) => (
 
-            </ul>
-        </div>
+                            <li key={todo.id}>{index + 1}. {todo.description}
+                                <button>
+                                    Borrar
+                                </button>
+
+                            </li>
+
+                        ))
+                    }
+
+                </ul>
+            </div>
+            <div className="col-2">
+
+                <h4>Agregador ToDO</h4>
+                <hr/>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    name="description"
+                    autoComplete="off"
+                    placeholder="Aprender ..."
+
+                    />
+                    <button type="submit">
+                        Agregar
+                    </button>
+
+                </form>
+
+            </div>
+        </>
+
     )
 }
 
--- reducers/toDoReducer.js
+++ reducers/toDoReducer.js
@@ -1,9 +1,9 @@
-const toDoReducer  = (state=[],action) =>{
-    switch(action.type){
-/*         case 'add':
-            break; */
-    default:
-        return state;
-        }
+const toDoReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'add':
+            return [...state, action.payload]
+        default:
+            return state;
+    }
 }
 export default toDoReducer;
